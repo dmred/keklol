537ef159552783ca459cfad7a086f7901b85ff1e
Dima Red <dmredchenko@gmail.com>
2019-04-25 11:47:21 +0300

    keklol

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 //const carbonCli = require("carbon-now-cli");
 const exec = require("child_process").exec;
-const createTempFile = require("fs").writeFileSync;
-const removeTempFile = require("fs").unlinkSync;
+const createTempFile = require("fs").writeFile;
+const removeTempFile = require("fs").unlink;
 
-const code = "const kek = { one: 1, two: 2, three: 3, four: 4, five: 5}";
+const code =
+  "const kek = { one: 1, two: 2, three: 3, four: 4, five: 5, dasdadasdasdasdasdasdfafssdas:32}";
 const tempFile = "temp.js";
 
-createTempFile(tempFile, code, function(err) {
-  if (err) throw err;
-  console.log("File is created successfully.");
+createTempFile(tempFile, code, {}, () => {
+  console.log("created");
+  exec(
+    `node ./node_modules/carbon-now-cli/cli.js index.js`,
+    (error, stdout, stderr) => {
+      removeTempFile(tempFile, () => {
+        console.log("removed");
+      });
+      //console.log(error, stdout, stderr);
+    }
+  );
 });
 
-exec(
-  `node ./node-modules/carbon-now-cli/cli.js ${tempFile}`,
-  (error, stdout, stderr) => {
-   // console.log(error, stdout, stderr);
-  }
-);
+// const code = "const kek = { one: 1, two: 2, three: 3, four: 4, five: 5}";
+// const tempFile = "temp.js";
 
-removeTempFile(tempFile, code, function(err) {
-  if (err) throw err;
-  console.log("File is removed successfully.");
-});
+// createTempFile(tempFile, code, function(err) {
+//   if (err) throw err;
+//   console.log("File is created successfully.");
+// });
+
+// setTimeout(() => {}, 5000);
+
+// removeTempFile(tempFile, function(err) {
+//   if (err) throw err;
+//   console.log("File is removed successfully.");
+// });
+
+// exec(
+//   `node ./node-modules/carbon-now-cli/cli.js ${tempFile}`,
+//   (error, stdout, stderr) => {
+//     console.log(error, stdout, stderr);
+//   }
+// );
+
+// removeTempFile(tempFile, code, function(err) {
+//   if (err) throw err;
+//   console.log("File is removed successfully.");
+// });
