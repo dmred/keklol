3f3c742f29696e5008942796e36240f78d86797d
Dima Red <dmredchenko@gmail.com>
2019-04-25 13:46:30 +0300

    dasd

---
+++ code-screenshot.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const Screenshot = require("..");
+
+const draw1 = async () => {
+  try {
+    const self = fs.readFileSync(path.resolve(__dirname, "./js.js"), "utf8");
+    let screenshot = new Screenshot(Screenshot.measureFullText(self));
+    await screenshot.drawText(self, "js");
+    await screenshot.writeFile(path.resolve(__dirname, "js.png"));
+    console.log("wrote out the png file to out.png");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+draw1();
